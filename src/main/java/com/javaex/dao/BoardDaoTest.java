fd77198c29590c25f1711a46fefcef330c7240a2
User <User@172.20.40.73>
2024-01-24 15:15:04 +0900

    -branch 생성

---
+++ src/main/java/com/javaex/dao/BoardDaoTest.java
@@ -0,0 +1,54 @@
+package com.javaex.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardDaoTest {
+
+	@Test
+	public void testGetList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSubList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTotalRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHitUp() {
+		fail("Not yet implemented");
+	}
+
+}
